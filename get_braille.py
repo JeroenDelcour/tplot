1e9e1a1c36c6ac79c010201a111af90790e29e00
jeroendelcour <jeroendelcour@gmail.com>
2018-06-05 22:32:45 +0200

    improved wording of docstring, added schematic illustrating dot ordering, added another example

--- get_braille.py
+++ get_braille.py
@@ -2,22 +2,26 @@
 
 def get_braille(s):
     """
-    `s` specifies which dots in the desired braille character must be on ('1') and which must be off ('0').
-    Dots in the 2x8 braille matrix are ordered top-down, left-to-right.
-    The order of the '1's and '0's in `s` correspond to this.
+    `s` is a string of '1's and '0's specifying which dots in the desired braille character must be on ('1') and which must be off ('0').
+    Dots in the 2x8 braille matrix are ordered top-down, left-to-right, i.e:
+
+    a •• e
+    b •• f
+    c •• g
+    d •• h
 
     Schematic example:
 
-    ▪    10
-     ▪   01
-    ▪▪ = 11
-     ▪   01
+    •    10
+     •   01
+    •• = 11
+     •   01
 
     ⢵ = '10100111'
     
     More examples:
     '10000000' = ⠁ (only top left dot)
-    '11001111' = ⢻
+    '00011110' = ⡸
     '11001111' = ⢻
     '11111111' = ⣿
     '00000000' = ⠀ (empty braille character)
