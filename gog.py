bcddf262efdbcea15b67ab6827c54d45d7dc2559
Jeroen Delcour <jeroen.delcour@orangenxt.com>
2019-03-28 22:14:56 +0100

    Discrete scaling

--- gog.py
+++ gog.py
@@ -2,11 +2,6 @@
 # Geometric shapes unicode: https://en.wikipedia.org/wiki/Geometric_Shapes
 # Useful: https://towardsdatascience.com/a-comprehensive-guide-to-the-grammar-of-graphics-for-effective-visualization-of-multi-dimensional-1f92b4ed4149
 
-# continuous: float
-# discrete: int
-# nominal: str
-# dichotomous: bool
-
 import math
 from bisect import bisect
 from itertools import repeat
@@ -16,36 +11,56 @@ dataset = {
     'Aardvark': [2, 1, 6, 4, 5],
     'Betadine': [3, 2, 3, 5, 10],
     'Cylindrical': [7, 1, 5, 15, -5],
-    'Dirk': ['a', 'a', 'a', 'b', 'b']
+    'Dirk': ['aantonend', 'afstoten', 'aardig', 'bij', 'bezig']
 }
 
 
-def linear_scale(V1, V2, W1, W2):
-    """ Returns transform function from vector space V to vector space W. """
-    V_range = V2 - V1
-    W_range = W2 - W1
+def generate_scale(data, W1, W2):
+    def linear(data, W1, W2):
+        """ Returns transform function from vector space V to vector space W. """
+        V1 = min(data)
+        V2 = max(data)
+        V_range = V2 - V1
+        W_range = W2 - W1
 
-    if V_range == 0:
-        return lambda x: W1
+        if V_range == 0:
+            return lambda: W1
 
-    def scale(x):
-        try:
-            return (W_range*(xi-V1)/V_range+W1 for xi in x)
-        except:
-            return W_range*(x-V1)/V_range+W1
-    return scale
+        def linear(v):
+            try:
+                return (W_range*(vi-V1)/V_range+W1 for vi in v)
+            except:
+                return W_range*(v-V1)/V_range+W1
+        return linear
+
+    def discrete(data, W1, W2):
+        s = sorted(set(data))
+        if W2-W1 <= len(s):
+            sign = +1 if W2 >= W1 else -1
+            d = {v: W1+i*sign for i, v in enumerate(s)}
+        else:
+            linear_scaler = linear(range(len(s)), W1, W2)
+            d = {v: linear_scaler(i) for i, v in enumerate(s)}
+
+        def discrete(v):
+            return d[v]
+        return discrete
+
+    try:
+        return linear(data, W1, W2)
+    except TypeError:
+        return discrete(data, W1, W2)
 
 
-scale_gens = {'linear': linear_scale}
+def geom_point(marker=None):
+    def geom_point_fn(canvas, scaled_data, marker=marker):
+        for x, y, m in zip(scaled_data['x'](), scaled_data['y'](), scaled_data['marker']()):
+            canvas[int(y)][int(x)] = m if not marker else marker
+        return canvas
+    return geom_point_fn
 
 
-def geom_point(canvas, scaled_data):
-    for x, y, marker in zip(scaled_data['x'](), scaled_data['y'](), scaled_data['marker']()):
-        canvas[int(y)][int(x)] = marker
-    return canvas
-
-
-def geom_path(canvas, scaled_data):
+def geom_path(marker=None):
 
     def plot_line(x0, y0, x1, y1):
         """ Plot line using Bresenham algorithm. Yields (x, y). """
@@ -74,14 +89,16 @@ def geom_path(canvas, scaled_data):
                 D -= 2*dx
             D += 2*dy
 
-    X, Y = scaled_data['x'](), scaled_data['y']()
-    x0, y0 = next(X), next(Y)
-    for x1, y1, marker in zip(X, Y, scaled_data['marker']()):
-        for x, y in plot_line(int(x0), int(y0), int(x1), int(y1)):
-            canvas[y][x] = marker
-        x0, y0 = x1, y1
+    def geom_path_fn(canvas, scaled_data, marker=marker):
+        X, Y = scaled_data['x'](), scaled_data['y']()
+        x0, y0 = next(X), next(Y)
+        for x1, y1, m in zip(X, Y, scaled_data['marker']()):
+            for x, y in plot_line(int(x0), int(y0), int(x1), int(y1)):
+                canvas[y][x] = m if not marker else marker
+            x0, y0 = x1, y1
 
-    return canvas
+        return canvas
+    return geom_path_fn
 
 
 aes_defaults = {'x': 0,
@@ -98,7 +115,7 @@ def aes(x=None,
 
 
 class Plot:
-    def __init__(self, data, mapping, scale='linear', width=64, height=32):
+    def __init__(self, data, mapping, width=64, height=32):
 
         def best_ticks(min_, max_, most):
             # find step size
@@ -117,23 +134,31 @@ class Plot:
             start = step * math.floor(abs(min_) / step) * sign
             return [start+i*step for i in range(int((max_-start)/step)+1)]
 
-        def y_aesthetics(data, height, scale_gen):
-            Y_min, Y_max = min(data['y']), max(data['y'])
-            y_scale = scale_gen(Y_min, Y_max, -3, -height)
-            y_labels = best_ticks(Y_min, Y_max, most=int((height-2)/2))
+        def y_aesthetics(data, height):
+            y_scale = generate_scale(data['y'], -3, -height)
+            if y_scale.__name__ == "discrete":
+                y_labels = set(data['y'])
+            else:
+                y_labels = best_ticks(min(data['y']), max(
+                    data['y']), most=int((height-2)/2))
             y_ticks = [int(y_scale(y)) for y in y_labels]
-            y_labels = ['{:.6g}'.format(l) for l in y_labels]
+            if y_scale.__name__ != "discrete":
+                y_labels = ['{:.6g}'.format(l) for l in y_labels]
             # max length of y label strings
-            margin_left = max((len(l) for l in y_labels)) + 1
+            margin_left = max((len(l) for l in y_labels)) + 2
             return y_scale, y_ticks, y_labels, margin_left
 
-        def x_aesthetics(data, width, scale_gen, margin_left):
-            X_min, X_max = min(data['x']), max(data['x'])
-            x_scale = scale_gen(X_min, X_max, margin_left, width-1)
-            x_labels = best_ticks(
-                X_min, X_max, most=int((width-margin_left)/(6+1)))
+        def x_aesthetics(data, width, margin_left):
+            x_scale = generate_scale(
+                data['x'], margin_left, width-1)
+            if x_scale.__name__ == "discrete":
+                x_labels = set(data['x'])
+            else:
+                x_labels = best_ticks(
+                    min(data['x']), max(data['x']), most=int((width-margin_left)/(6+1)))
             x_ticks = [int(x_scale(x)) for x in x_labels]
-            x_labels = ['{:.6g}'.format(l) for l in x_labels]
+            if x_scale.__name__ != "discrete":
+                x_labels = ['{:.6g}'.format(l) for l in x_labels]
             return x_scale, x_ticks, x_labels
 
         def draw_axes(canvas, margin_left):
@@ -173,11 +198,14 @@ class Plot:
             center = int(width/2)
             start = center - int(len(label)/2)
             for l, x in zip(label, range(start, start+len(label)+1)):
-                canvas[-1][x] = l
+                canvas[-1][margin_left+x] = l
             return canvas
 
         def map_aes(data, mapping, defaults):
             """ Indexes data by aesthetic, inserting default values. """
+            for v in mapping.values():
+                if v is not None and not v in data:
+                    raise KeyError("'{}' not in data".format(v))
             dim_lengths = list(
                 map(len, (v for k, v in data.items() if v is not None)))
             if not dim_lengths.count(dim_lengths[0]) == len(dim_lengths):
@@ -186,7 +214,8 @@ class Plot:
                     for dim, key in mapping.items()}
 
         data = map_aes(data, mapping, aes_defaults)
-        scales = {dim: lambda v: v for dim in ('x', 'y', 'marker')}
+        scales = {dim: lambda v: v for dim in (
+            'x', 'y', 'marker')}  # identity functions
 
         # aesthetics order:
         # 1. marker/color/etc. (anything that needs a legend)
@@ -196,13 +225,18 @@ class Plot:
         margin_left = 0
         if mapping['y']:
             scales['y'], y_ticks, y_labels, margin_left = y_aesthetics(
-                data, height, scale_gens[scale])
+                data, height)
+            if scales['y'].__name__ == "discrete_scaler":
+                # resize plot to fit
+                min_scale_height = -scales['y'](max(data['y']))
+                min_y_label_height = len(mapping['y'])+1
+                height = max(min_scale_height, min_y_label_height)
         else:
             height = 4
 
         if mapping['x']:
             scales['x'], x_ticks, x_labels = x_aesthetics(
-                data, width, scale_gens[scale], margin_left)
+                data, width, margin_left)
         else:
             width = margin_left + 2
             scales['x'] = lambda x: margin_left + 1
@@ -234,4 +268,5 @@ class Plot:
         return '\n'.join((''.join(row) for row in self.canvas))
 
 
-print(Plot(dataset, aes(x='Aardvark', y='Cylindrical')) + geom_path)
+print(Plot(dataset, aes(y='Aardvark', x='Dirk')) +
+      geom_point(marker='+'))
